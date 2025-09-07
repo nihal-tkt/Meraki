92be3ba34f58ac34b37c0cfd04d0c96f3db541d4
jophyjames12 <jophyjames12@gmail.com>
2025-09-07 06:55:30 +0530

    more fixes

--- Meraki/backend/strategies/facebookStrategy.js
+++ Meraki/backend/strategies/facebookStrategy.js
@@ -6,7 +6,24 @@ const FACEBOOK_APP_ID = process.env.FACEBOOK_APP_ID;
 const FACEBOOK_APP_SECRET = process.env.FACEBOOK_APP_SECRET;
 
 
-
+export default passport.use(new FacebookStrategy({
+    clientID: FACEBOOK_APP_ID,
+    clientSecret: FACEBOOK_APP_SECRET,
+    callbackURL: "/api/v1/auth/facebook/callback",
+    profileFields: ['id', 'displayName', 'emails'] // specify fields you want to fetch
+}, async (accessToken, refreshToken, profile, done) => {
+    const existingUser = await User.findOne({ facebookId: profile.id });
+    if (existingUser) {
+        return done(null, existingUser);
+    }
+    const newUser = await new User({
+        fullName: profile.displayName,
+        email: profile.emails[0].value,
+        role: 'student', // Default role or based on your logic
+        facebookId: profile.id,
+    }).save();
+    done(null, newUser);
+}));
 
 passport.serializeUser((user, done) => {
     done(null, user.id);
@@ -17,3 +34,4 @@ passport.deserializeUser((id, done) => {
         done(null, user);
     });
 });
+
