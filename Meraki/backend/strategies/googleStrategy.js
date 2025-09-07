af3e13ca62205f03a54a3b0e9334c1d05cb692d7
jophyjames12 <jophyjames12@gmail.com>
2025-09-07 06:51:41 +0530

    fixx

--- Meraki/backend/strategies/googleStrategy.js
+++ Meraki/backend/strategies/googleStrategy.js
@@ -5,28 +5,6 @@ import dotenv from "dotenv";
 dotenv.config();
 import GoogleStrategy from 'passport-google-oauth20';
 
-const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
-
-export default passport.use(new GoogleStrategy({
-    clientID: GOOGLE_CLIENT_ID,
-    clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/v1/auth/google/callback",
-}, async (accessToken, refreshToken, profile, done) => {
-    const existingUser = await User.findOne({ googleId: profile.id });
-    if (existingUser) {
-        return done(null, existingUser);
-    }
-
-    const role = profile._json.session?.role || 'student';
-    const newUser = await new User({
-        fullName: profile.displayName,
-        email: profile.emails[0].value,
-        role: 'student', // Default role or based on your logic
-        googleId: profile.id
-    }).save();
-    done(null, newUser);
-}));
 
 
 passport.serializeUser((user, done) => {
